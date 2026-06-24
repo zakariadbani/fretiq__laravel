8d17024c19557fce22a9b9e7b86bbb9f92a75034
zakaria desktop <zakariadbani@gmail.com>
2026-06-24 12:39:42 +0100

    modif

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use Livewire\Livewire;
 use App\Core\KTBootstrap;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\ServiceProvider;
 
@@ -92,11 +90,5 @@ class AppServiceProvider extends ServiceProvider
         Builder::defaultStringLength(191);
 
         KTBootstrap::init();
-
-        if (app()->environment('production')) {
-            Livewire::setUpdateRoute(function ($handle) {
-                return Route::post('/starterkit/metronic/laravel/livewire/update', $handle);
-            });
-        }
     }
 }
